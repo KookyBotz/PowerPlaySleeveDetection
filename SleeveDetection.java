9b2363df5904d4734d63e27d2248696667ec7853
Mason Stuart <55424697+ProDCG@users.noreply.github.com>
2022-10-03 23:08:24 -0700

    Update SleeveDetection.java

--- SleeveDetection.java
+++ SleeveDetection.java
@@ -25,11 +25,14 @@ public class SleeveDetection extends OpenCvPipeline {
         RIGHT
     }
 
+    // TOPLEFT anchor point for the bounding box
     private static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(145, 168);
 
+    // Width and height for the bounding box
     public static int REGION_WIDTH = 30;
     public static int REGION_HEIGHT = 50;
 
+    // Lower and upper boundaries for colors
     private static final Scalar
             lower_yellow_bounds  = new Scalar(200, 200, 0, 255),
             upper_yellow_bounds  = new Scalar(255, 255, 130, 255),
@@ -38,40 +41,48 @@ public class SleeveDetection extends OpenCvPipeline {
             lower_magenta_bounds = new Scalar(170, 0, 170, 255),
             upper_magenta_bounds = new Scalar(255, 60, 255, 255);
 
+    // Color definitions
     private final Scalar
             YELLOW  = new Scalar(255, 255, 0),
             CYAN    = new Scalar(0, 255, 255),
             MAGENTA = new Scalar(255, 0, 255);
 
+    // Percent and mat definitions
     private double yelPercent, cyaPercent, magPercent;
-
     private Mat yelMat = new Mat(), cyaMat = new Mat(), magMat = new Mat(), blurredMat = new Mat();
 
+    // Anchor point definitions
     Point sleeve_pointA = new Point(
             SLEEVE_TOPLEFT_ANCHOR_POINT.x,
             SLEEVE_TOPLEFT_ANCHOR_POINT.y);
-
     Point sleeve_pointB = new Point(
             SLEEVE_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
             SLEEVE_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
 
+    // Running variable storing the parking position
     private volatile ParkingPosition position = ParkingPosition.LEFT;
 
     @Override
     public Mat processFrame(Mat input) {
+        // Noise reduction
         Imgproc.blur(input, blurredMat, new Size(5, 5));
         blurredMat = blurredMat.submat(new Rect(sleeve_pointA, sleeve_pointB));
         
+        // Gets channels from given source mat
         Core.inRange(blurredMat, lower_yellow_bounds, upper_yellow_bounds, yelMat);
         Core.inRange(blurredMat, lower_cyan_bounds, upper_cyan_bounds, cyaMat);
         Core.inRange(blurredMat, lower_magenta_bounds, upper_magenta_bounds, magMat);
 
+        // Gets color specific values
         yelPercent = Core.countNonZero(yelMat);
         cyaPercent = Core.countNonZero(cyaMat);
         magPercent = Core.countNonZero(magMat);
 
+        // Calculates the highest amount of pixels being covered on each side
         double maxPercent = Math.max(yelPercent, Math.max(cyaPercent, magPercent));
 
+        // Checks all percentages, will highlight bounding box in camera preview
+        // based on what color is being detected
         if (maxPercent == yelPercent) {
             position = ParkingPosition.LEFT;
             Imgproc.rectangle(
@@ -104,6 +115,7 @@ public class SleeveDetection extends OpenCvPipeline {
         return input;
     }
 
+    // Returns an enum being the current position where the robot will park
     public ParkingPosition getPosition() {
         return position;
     }
